9263951e469b84ba3b9bfde6b40e63d9f8548901
Vanessa Santana <vanessah4money@gmail.com>
2021-04-13 22:39:11 -0300

    test: class 3 working with stubs

---
+++ aula03-spies/src/fibonacci.test.js
@@ -0,0 +1,29 @@
+const Fibonacci = require('./fibonacci');
+const sinon = require('sinon');
+const assert = require('assert');
+
+//Fibonacci: O proximo valor corresponde a sooma dos anteriores
+( async ()=>{
+    {
+        const fibonacci = new Fibonacci();
+        const spy = sinon.spy(fibonacci, fibonacci.execute.name);
+        for await (const i of fibonacci.execute(3)){};
+        
+        const expectedCallCount = 4;
+        assert.deepStrictEqual(spy.callCount, expectedCallCount);
+    }
+    {
+        const fibonacci = new Fibonacci();
+        const spy = sinon.spy(fibonacci, fibonacci.execute.name);  
+        const [...results] = fibonacci.execute(5);
+        const {args} = spy.getCall(2);
+        const expectedResult = [0,1,1,2,3];
+        const expectedParams = Object.values({
+            input:3,
+            current: 1,
+            next: 2
+        });
+        assert.deepStrictEqual(args,expectedParams);
+        assert.deepStrictEqual(results,expectedResult);
+    }
+})();
